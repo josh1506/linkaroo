95041054cbe7507cb6bfa6c50d82576c02bfecc1
josh1506 <jmjabor@gmail.com>
2024-02-13 19:41:54 +1300

    [Adjustment][Joshua] Add additional fields in user form

--- app/users/forms/registration.py
+++ app/users/forms/registration.py
@@ -4,8 +4,10 @@ from django.contrib.auth.models import User
 
 
 class RegistrationForm(UserCreationForm):
-    confirm_password = forms.PasswordInput()
+    first_name = forms.CharField(max_length=255, required=True)
+    last_name = forms.CharField(max_length=255, required=True)
+    email = forms.EmailField(required=True)
 
     class Meta:
         model = User
-        fields = ['username', 'password1', 'password2']
+        fields = ['username', 'first_name', 'last_name', 'email', 'password1', 'password2']
