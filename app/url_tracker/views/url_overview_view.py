f86cc6ce6dccd1bab1ecf12eb45f59012d878720
josh1506 <jmjabor@gmail.com>
2024-02-15 02:41:04 +0800

    [Adjustment][Joshua] Enhanced chart data for Total Links in overview page

--- app/url_tracker/views/url_overview_view.py
+++ app/url_tracker/views/url_overview_view.py
@@ -2,7 +2,7 @@ import json
 
 from django.views import generic
 
-from app.url_tracker.utils import get_latest_7_days_total_clicks, get_total_active_urls, get_top_5_links
+from app.url_tracker.utils import get_latest_7_days_total_clicks, get_total_active_urls, get_top_5_links, get_links_created_day_range
 
 
 class UrlOverviewTemplateView(generic.TemplateView):
@@ -14,40 +14,7 @@ class UrlOverviewTemplateView(generic.TemplateView):
         kwargs['top_links'] = get_top_5_links(user_url)
         kwargs['total_active_url'] = json.dumps(get_total_active_urls(user_url))
         kwargs['latest_total_clicks'] = json.dumps(get_latest_7_days_total_clicks(user_url))
-
-        kwargs['total_links_data'] = json.dumps({
-            'active_links_count': 0,
-            'recent_7_days_created': [
-                ['Acrocanthosaurus (top-spined lizard)', 12.2],
-                ['Albertosaurus (Alberta lizard)', 9.1],
-                ['Allosaurus (other lizard)', 12.2],
-                ['Apatosaurus (deceptive lizard)', 22.9],
-                ['Archaeopteryx (ancient wing)', 0.9],
-                ['Argentinosaurus (Argentina lizard)', 36.6],
-                ['Baryonyx (heavy claws)', 9.1],
-                ['Brachiosaurus (arm lizard)', 30.5],
-                ['Ceratosaurus (horned lizard)', 6.1],
-                ['Coelophysis (hollow form)', 2.7],
-                ['Compsognathus (elegant jaw)', 0.9],
-                ['Deinonychus (terrible claw)', 2.7],
-                ['Diplodocus (double beam)', 27.1],
-                ['Dromicelomimus (emu mimic)', 3.4],
-                ['Gallimimus (fowl mimic)', 5.5],
-                ['Mamenchisaurus (Mamenchi lizard)', 21.0],
-                ['Megalosaurus (big lizard)', 7.9],
-                ['Microvenator (small hunter)', 1.2],
-                ['Ornithomimus (bird mimic)', 4.6],
-                ['Oviraptor (egg robber)', 1.5],
-                ['Plateosaurus (flat lizard)', 7.9],
-                ['Sauronithoides (narrow-clawed lizard)', 2.0],
-                ['Seismosaurus (tremor lizard)', 45.7],
-                ['Spinosaurus (spiny lizard)', 12.2],
-                ['Supersaurus (super lizard)', 30.5],
-                ['Tyrannosaurus (tyrant lizard)', 15.2],
-                ['Ultrasaurus (ultra lizard)', 30.5],
-                ['Velociraptor (swift robber)', 1.8]
-            ],
-        })
+        kwargs['total_links_data'] = json.dumps(get_links_created_day_range(user_url, 7))
 
         kwargs['total_clicks_data'] = json.dumps({
             'clicks_count': 0,
