52e229d326addb0ee613b04c30ca2fa411ec5239
josh1506 <jmjabor@gmail.com>
2024-02-19 12:52:49 +0800

    [Adjustment][Joshua] Add 'url_owner' field to UrlTracker model and create URL Tracker page

--- app/url_tracker/models/models_url_tracker.py
+++ app/url_tracker/models/models_url_tracker.py
@@ -1,7 +1,11 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
 from .models_url import URL
 from app.utils.model import Model
+
+
+User = get_user_model()
 
 
 class URLTracker(Model):
@@ -11,6 +15,18 @@ class URLTracker(Model):
         ordering = ["-created_at"]
 
     url = models.ForeignKey(URL, on_delete=models.CASCADE, related_name="tracker", verbose_name="URL")
+    url_owner = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="owned_url_tracker",
+        verbose_name="URL Owner",
+        blank=True,
+        null=True
+    )
 
     def __str__(self):
         return self.url.title
+
+    def save(self, *args, **kwargs):
+        self.url_owner = self.url.owner
+        super().save(*args, **kwargs)
--- app/url_tracker/views/__init__.py
+++ app/url_tracker/views/__init__.py
@@ -4,6 +4,7 @@ from app.url_tracker.views.url_list_view import UrlListView
 from app.url_tracker.views.url_update_view import UrlUpdateView
 from app.url_tracker.views.url_create_view import URLCreateView
 from app.url_tracker.views.url_overview_view import UrlOverviewTemplateView
+from app.url_tracker.views.url_tracker_list_view import UrlTrackerListView
 
 
-__all__ = [URLCreateView, UrlOverviewTemplateView, UrlDetailView, UrlListView, UrlUpdateView]
+__all__ = [URLCreateView, UrlOverviewTemplateView, UrlDetailView, UrlListView, UrlUpdateView, UrlTrackerListView]
