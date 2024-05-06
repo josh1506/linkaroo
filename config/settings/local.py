6701d877cf8e151ef30343fb021314354fd9861e
josh1506 <jmjabor@gmail.com>
2024-05-06 22:42:20 +0800

    [Adjustment][Joshua] Improve file structure

--- config/settings/local.py
+++ config/settings/local.py
@@ -6,7 +6,7 @@ dotenv_path = os.path.join(os.path.abspath(__file__), '.env')
 load_dotenv()
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
-ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS').split(" ")
+ALLOWED_HOSTS = ["*"]
 
 DEBUG = os.environ.get('DEBUG')
 PRODUCTION = os.environ.get('PRODUCTION')
@@ -27,12 +27,12 @@ DATABASES = {
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'app/static'),
-    os.path.join(BASE_DIR, 'app/media')
+    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'media')
 ]
 
-STATIC_URL = 'app/static/'
-MEDIA_URL = 'app/media/'
+STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'app/staticfiles')
-MEDIA_ROOT = os.path.join(BASE_DIR, 'app/mediafiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
